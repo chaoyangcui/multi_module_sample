17d52c7f5d1b1fb14d4d51d712d093e3c7b36617
superPMonkey <cui27387@163.com>
2017-09-11 17:45:19 +0800

    #ADD 1.Unsafe

--- __java-sample/src/main/java/GlobalConf.java
+++ __java-sample/src/main/java/GlobalConf.java
@@ -9,14 +9,18 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class GlobalConf {
 
+    static {
+        System.out.println("static block.");
+    }
+
     public static void main(String[] args) {
         ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
         map.put("key", "value");
 
-        System.out.println(Integer.numberOfLeadingZeros(0));
-        System.out.println(Integer.numberOfLeadingZeros(1));
-        System.out.println(Integer.numberOfLeadingZeros(2));
-        System.out.println(Integer.numberOfLeadingZeros(3));
+        // System.out.println(Integer.numberOfLeadingZeros(0));
+        // System.out.println(Integer.numberOfLeadingZeros(1));
+        // System.out.println(Integer.numberOfLeadingZeros(2));
+        // System.out.println(Integer.numberOfLeadingZeros(3));
 
         // System.out.println(Integer.MAX_VALUE);
         // System.out.println(Integer.parseInt("7fffffff", 16));
@@ -32,15 +36,15 @@ public class GlobalConf {
     }
 
 
-    private static final int MAXIMUM_CAPACITY = 1 << 30;
-    private static final int tableSizeFor(int c) {
-        int n = c - 1;
-        n |= n >>> 1;
-        n |= n >>> 2;
-        n |= n >>> 4;
-        n |= n >>> 8;
-        n |= n >>> 16;
-        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
-    }
+    // private static final int MAXIMUM_CAPACITY = 1 << 30;
+    // private static final int tableSizeFor(int c) {
+    //     int n = c - 1;
+    //     n |= n >>> 1;
+    //     n |= n >>> 2;
+    //     n |= n >>> 4;
+    //     n |= n >>> 8;
+    //     n |= n >>> 16;
+    //     return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    // }
 
 }
