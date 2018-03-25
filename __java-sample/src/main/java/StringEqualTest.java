5760cd6ac9bc326731a4239b364a95e09cd220d7
eric <cui27387@163.com>
2018-03-25 18:22:55 +0800

    update

---
+++ __java-sample/src/main/java/StringEqualTest.java
@@ -0,0 +1,17 @@
+/**
+ * @author Eric Cui
+ * <p>
+ * Created by Intellij IDEA.
+ * Date  : 2018/3/7 23:37
+ * Desc  : 描述信息
+ */
+public class StringEqualTest {
+    public static void main(String[] args) {
+        String s1 = "Programming";
+        String s2 = new String("Programming");
+        String s3 = "Program" + "ming";
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s1 == s1.intern());
+    }
+}
